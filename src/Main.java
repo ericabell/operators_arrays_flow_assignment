7307f193db7f7d173eb9dcb3c79c56106bb62e4c
Eric Abell <ericabell@users.noreply.github.com>
2017-07-22 22:36:19 -0500

    Implement convert first arg to int and branch on that int

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("begin");
 
-        /* check for empty args */
+        /* check for empty args, print list of args in non-empty */
         if(args.length == 0) {
             System.out.println("Didn't pass any args");
         }
         else {
-            System.out.println("printing arguments passed into main");
+            System.out.println(" * Printing arguments passed into main");
             for (int i = 0; i < args.length; i++) {
-                System.out.println("Argument " + i + ": " + args[i]);
+                System.out.println("   Argument " + i + ": " + args[i]);
             }
         }
+
+        /* Assume first argument is a number, convert to int */
+        int firstArgument = Integer.parseInt(args[0]);
+
+        /* Branches on one of the arguments */
+        System.out.println(" * Branching on first argument int.");
+        if( firstArgument == 1 ){
+            System.out.println("   Hey! First argument was 1.");
+        }
+        else {
+            System.out.println("   First argument was not 1.");
+        }
+
+
 
     }
 }
