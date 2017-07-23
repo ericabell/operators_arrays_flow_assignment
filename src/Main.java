9bcf4db3edad110c3193eafad0185aeb6ce8634b
Eric Abell <ericabell@users.noreply.github.com>
2017-07-22 22:29:00 -0500

    Implement printing command line arguments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("begin");
+
+        /* check for empty args */
+        if(args.length == 0) {
+            System.out.println("Didn't pass any args");
+        }
+        else {
+            System.out.println("printing arguments passed into main");
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Argument " + i + ": " + args[i]);
+            }
+        }
+
     }
 }
