fd6a960d74bbbd30b964c6fab01e6a8a188e3b2f
Eric Abell <ericabell@users.noreply.github.com>
2017-07-22 22:44:22 -0500

    Implement switch on second argument.
    Added check for sufficient number of args passed into program
    via the command line.

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,40 @@ public class Main {
         System.out.println("begin");
 
         /* check for empty args, print list of args in non-empty */
-        if(args.length == 0) {
-            System.out.println("Didn't pass any args");
+        if(args.length < 2) {
+            System.out.println("Didn't pass enough args");
         }
         else {
             System.out.println(" * Printing arguments passed into main");
             for (int i = 0; i < args.length; i++) {
                 System.out.println("   Argument " + i + ": " + args[i]);
             }
-        }
 
-        /* Assume first argument is a number, convert to int */
-        int firstArgument = Integer.parseInt(args[0]);
+            /* Assume first argument is a number, convert to int */
+            int firstArgument = Integer.parseInt(args[0]);
 
-        /* Branches on one of the arguments */
-        System.out.println(" * Branching on first argument int.");
-        if( firstArgument == 1 ){
-            System.out.println("   Hey! First argument was 1.");
-        }
-        else {
-            System.out.println("   First argument was not 1.");
+            /* Branches on one of the arguments */
+                System.out.println(" * Branching on first argument int.");
+                if( firstArgument == 1 ){
+                    System.out.println("   Hey! First argument was 1.");
+                }
+                else {
+                    System.out.println("   First argument was not 1.");
+                }
+
+            /* switch on the second argument, which is a string */
+                System.out.println(" * Switch on second argument.");
+                switch(args[1]) {
+                    case "one":
+                        System.out.println("   second argument was one");
+                        break;
+                    case "two":
+                        System.out.println("   second argument was two");
+                    case "three":
+                        System.out.println("   second argument was three");
+                    default:
+                        System.out.println("   second argument was not one, two, or three");
+                }
         }
 
 
